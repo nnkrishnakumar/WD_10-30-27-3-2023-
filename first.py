78364da1c12560b8de16677daf190ef3b2bd6259
Krishna Kumar <nnkrishna714@gmail.com>
2023-04-05 11:16:04 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,22 @@
+print("hello world")
+print('I am krishna Kumar')
+print("""I am in MCA final year""")
+print('''on 3rd i will be on leave''')
+
+
+print("Hi this is 'DU'")
+print('Hi this is "DU"')
+print("Hi this is \"DU\"")
+print("I am swami \\nathan")
+
+print("I am krishna""kumar")
+print("I am krishna","kumar")
+print("Apple","banana","pineapple",sep="$\nkrishna\t")
+print("Apple","banana","pineapple",sep=",")
+print("Apple,""banana,""pineapple",sep=",")
+print("hello world",end="\n");print("hello world")
+print("hello world",end=" ");print("hello world", end=" ")
+print("this is krishna")
+print("Apple","banana","pineapple",sep=",",end="India")
+print("Apple","banana","pineapple",sep=",")
+print("this is krishna \n\bumar","India",end="\t")
