78d6b1a98876cdf5702fb8048ec0e27f89099716
Krishna Kumar <nnkrishna714@gmail.com>
2023-05-24 11:51:41 +0530

    commit

---
+++ package&Module/krishna.py
@@ -0,0 +1,25 @@
+#1st rule to import module
+import Package.Module1 as chi
+
+chi.add(2, 3)
+chi.div(3, 2)
+chi.sub(4,2)
+chi.mul(4, 2)
+print(chi.a)
+
+
+# 2nd rule to import 
+
+# from Package.Module1 import sub,div,mul
+
+# sub(2, 3)
+# div(3,4)
+# mul(3,4)
+
+# #3rd rule to import 
+
+# from Package.Module1 import *
+# sub(2, 3)
+# mul(3,4)
+# div(3, 4)
+# add(3, 4)
